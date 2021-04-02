796602d9ca3a019121e4bad7f78775658e14ad5a
Alessandro Venti <alessandro.venti@icloud.com>
2021-04-02 17:21:58 +0200

    Student table with form and remove - bonus

--- review-after-feedback/js/script.js
+++ review-after-feedback/js/script.js
@@ -1,106 +1,178 @@
 
-                              //ESERCIZIO TABELLA USANDO jQuery
-
-function $generateTd($object, $index) {
-     if ( $index == undefined) {
-          var $newtd = $(`<tr>
-                              <td class= "js-td">${$object.name}</td>
-                              <td class= "js-td">${$object.lastname}</td>
-                              <td class= "js-td">${$object.age}</td>
-                         </tr>`);  
-     }else {
-          var $newtd = $(`<tr>
-                              <td class= "js-td">${$object[$index].name}</td>
-                              <td class= "js-td">${$object[$index].lastname}</td>
-                              <td class= "js-td">${$object[$index].age}</td>
-                         </tr>`);  
-     }
-     return $newtd;
-}
+// ESERCIZIO TABELLA USANDO jQuery
 
 function $studentsDatabase() {
-     var $studentsArray = [
+     return $studentsArray = [
           { 'name' : 'Olga' , 'lastname' : 'Demina', 'age' : 29 },
           { 'name' : 'Gianluca', 'lastname' : 'Lomarco', 'age' : 35  },
           { 'name' : 'Giovanni' , 'lastname' : 'Cappelletti', 'age' : 30  }
      ]
-     for ( let $i = 0; $i < $studentsArray.length; $i++) {
-          $('tbody').append($generateTd($studentsArray, $i));
-     }
-     return $studentsArray;
 }
-     
+
 
 function $newStudent() {
-     var $newStudent = {
+     return {
           'name' : prompt('Inserisci il nome dello studente'),
           'lastname' : prompt('Inserisci il cognome dello studente'),
           'age' : parseInt(prompt("Inserisci l'eta dello studente"))
      }
-     $('tbody').append($generateTd($newStudent));
 }
+
+
+function $generateRowForDatabase(database) {
+     $('tbody').empty();
+     $.each(database,($index, $element) => {
+          var $newtd = $(`<tr>
+                              <td class= "js-td">${$element.name}</td>
+                              <td class= "js-td">${$element.lastname}</td>
+                              <td class= "js-td">${$element.age}</td>
+                         </tr>`);  
+          $('tbody').append($newtd);
+     });
+}
+
 
 function $table() {
-     $(document).ready($studentsDatabase);
-     var $button = $('#js-button-1');
-     $button.click($newStudent);
+     const $students = $studentsDatabase(); //-----> students
+     $generateRowForDatabase($students);  
+     $('#js-button-1').click(function() {
+          let $student = $newStudent(); //-----> new student               
+          $students.push($student);  //-----> update students data                  
+          $generateRowForDatabase($students);  //-----> refresh view          
+     });
 }
 
-$(document).ready($table);
+
+// $(document).ready($table);
 
 
 
-                                        //ESERCIZIO TABELLA USANDO JS VANILLA
 
 
 
+                                        //Esercizio tabella usando jQuery, usando il form e con tasto "remove"
 
-function generateTd(trTag, object, keyObject, index) {
-     var newtd = document.createElement('td');
-     trTag.append(newtd);
-     if (index == undefined) {
-          var tdText = document.createTextNode(object[keyObject]);
-     } else {
-          var tdText = document.createTextNode(object[index][keyObject]);
+
+function $studentsDatabase() {
+     return $studentsArray = [
+          { 'name' : 'Olga' , 'lastname' : 'Demina', 'age' : 29 },
+          { 'name' : 'Gianluca', 'lastname' : 'Lomarco', 'age' : 35  },
+          { 'name' : 'Giovanni' , 'lastname' : 'Cappelletti', 'age' : 30  }
+     ]
+}
+
+
+function $formToArray() {
+     var serialized = $('input').serializeArray();
+     let newStudentData = [];
+     $.each(serialized, function(index, element) {
+          newStudentData.push(element.value);
+     })
+     return newStudentData;
+}
+
+
+function $newStudent(formArray) {
+     return {
+          'name' : formArray[0],
+          'lastname' : formArray[1],
+          'age' : parseInt(formArray[2])
      }
-     newtd.appendChild(tdText);
-     return trTag;
 }
+
+
+function $generateRowForDatabase(database) {
+     $('tbody').empty();
+     $.each(database,($index, $element) => {
+          var $newtd = $(`<tr>
+                              <td class= "js-td">${$element.name}</td>
+                              <td class= "js-td">${$element.lastname}</td>
+                              <td class= "js-td">${$element.age}</td>
+                              </tr>`);  
+          $('tbody').append($newtd);
+     });
+}
+
+
+function remove(database) {
+          var databaseRemoved = database.pop();
+          $('tbody').empty();
+          $generateRowForDatabase(database);
+}
+
+
+function $table2() {
+     const $students = $studentsDatabase(); //-----> students data
+     $generateRowForDatabase($students);  //-----> View for students
+     $('#form').submit(function(event) {
+          event.preventDefault();
+          let formArray = $formToArray(); //----> get form-data array
+          let $student = $newStudent(formArray); //-----> new student data          
+          $students.push($student);  //-----> update students database                  
+          $generateRowForDatabase($students);  //-----> refresh view        
+     });
+     $('#js-button-3').click(function() {
+          remove($students); //---> remove the last student
+     })
+}
+
+
+$(document).ready($table2);
+
+
+
+
+
+
+                                        // ESERCIZIO TABELLA USANDO JS VANILLA
+
 
 function studentsDatabase() {
-     var studentsArray = [
+     return studentsArray = [
           { 'name' : 'Judy' , 'lastname' : 'Alvarez', 'age' : 29 },
           { 'name' : 'Panam', 'lastname' : 'Palmer', 'age' : 30  },
           { 'name' : 'Johnny' , 'lastname' : 'Silverhand', 'age' : 35  }
      ]
-     for ( let i = 0; i < studentsArray.length; i++) {
-          var element = document.getElementById('js-tbody');
-          var newtr = document.createElement('tr');
-          for( let key in studentsArray[i] ) {
-               element.appendChild(generateTd(newtr ,studentsArray, key, i));
-          }
-     }
-     return studentsArray;
 }
 
+
 function newStudent() {
-     var newStudent = {
+     return {
           'name' : prompt('Inserisci il nome dello studente'),
           'lastname' : prompt('Inserisci il cognome dello studente'),
           'age' : parseInt(prompt("Inserisci l'eta dello studente"))
      }
-     var element = document.getElementById('js-tbody');
-     var newtr = document.createElement('tr');
-     for (let key in newStudent) {
-          element.appendChild(generateTd(newtr ,newStudent, key));
-     }
 }
+
+
+function generateRowForDatabase(database) {
+     const tableId = document.getElementById('js-tbody');
+     tableId.innerHTML = "";
+     database.forEach(element => {
+          let newtr = document.createElement('tr');
+          for( let key in element) {
+               let newtd = document.createElement('td');
+               newtr.append(newtd);
+               let tdText = document.createTextNode(element[key]);
+               newtd.appendChild(tdText);
+               tableId.appendChild(newtr);
+          }
+     });      
+}
+
 
 function table() {
-     $(document).ready(studentsDatabase);
-     var button = document.getElementById('js-button-2');
-     button.addEventListener('click', newStudent);
-}
+     const students = studentsDatabase(); //-----> students
+     generateRowForDatabase(students);  
+     const button = document.getElementById('js-button-2');
+     button.addEventListener('click',function() {
+          let student = newStudent(); //-----> new student               
+          students.push(student);  //-----> update students data                  
+          generateRowForDatabase(students);  //-----> refresh view
+     }
+)};
 
-$(document).ready(table);
+
+// $(document).ready(table);
+
 
